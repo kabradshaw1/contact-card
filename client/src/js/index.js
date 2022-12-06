604305cb4fbf76c240cb230db651e6b14925875e
kabradshaw1 <itllbok82@gmail.com>
2022-12-06 14:14:35 -0500

    moved css to webpack

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,18 @@
+
+// Import modules
 import "./form";
 import "./submit";
+
+// Import Images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+
+// Import CSS files
+import "../css/index.css";
+
+window.addEventListener('load', function () {
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
