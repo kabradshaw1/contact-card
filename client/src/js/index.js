7aa0e961472844580e978004913575e7e3886f93
kabradshaw1 <itllbok82@gmail.com>
2022-12-12 11:35:04 -0500

    can now add entries

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,9 +1,9 @@
-import "./form";
-import "./submit";
+import { clearForm, toggleForm} from "./form";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { initdb, getDb, postDb } from './database';
 import { fetchCards } from "./cards";
+
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -25,3 +25,39 @@ window.addEventListener('load', function () {
 
     // Import CSS files
     import "../css/index.css";
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+  })
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let email = document.getElementById("email").value;
+let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+if (submitBtnToUpdate == false) {
+  postDb(name, email, phone, profile);
+} else {
+
+  fetchCards();
+    // Toggles the submit button back to POST functionality
+  submitBtnToUpdate = false;
+}
+
+// Clear form
+clearForm();
+// Toggle form
+toggleForm();
+// Reload the DOM
+fetchCards();
+});
