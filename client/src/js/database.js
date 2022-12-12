83cceca9f9756a774ba842c1f5acd7e8a60b352e
kabradshaw1 <itllbok82@gmail.com>
2022-12-12 12:58:41 -0500

    done with 19.3

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,8 +1,8 @@
 import { openDB } from 'idb';
 import 'regenerator-runtime/runtime';
 
-export const initdb = async () => {
-  // We are creating a new database named 'contact_db' which will be using version 1 of the database.
+export const initdb = async () =>
+// We are creating a new database named 'contact_db' which will be using version 1 of the database.
   openDB('contact_db', 1, {
     // Add our database schema if it has not already been initialized.
     upgrade(db) {
@@ -10,20 +10,22 @@ export const initdb = async () => {
         console.log('contacts store already exists');
         return;
       }
-      // Create a new object store for the data and give it a key name of 'id' which will increment automatically
+      // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
       db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
       console.log('contacts store created');
     }
-  })
-};
+  });
 
+// Exported READ function
+
+// Export a function we will use to GET to the database.
 export const getDb = async () => {
   console.log('GET from the database');
-  
-  // create a connection to the IndexedDB database and the version we want to use.
+
+  // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('contact_db', 1);
 
-  // Create a new transaction and speicify the store and data privileges.
+  // Create a new transaction and specify the store and data privileges.
   const tx = contactDb.transaction('contacts', 'readonly');
 
   // Open up the desired object store.
@@ -38,11 +40,13 @@ export const getDb = async () => {
   return result;
 };
 
-// Export a function we will use to POST to the database.
-export const postDb = async (name, email, phone, profile) => {
-  console.log('POST to the database');
+// EXPORTED CREATE function
 
-  // Create a connection to the database and specify the version we want to use.
+// Export a function we will use to POST to the database.
+export const postDb = async (name, email, phone, profile)  => {
+  console.log('Post to the database');
+
+  // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('contact_db', 1);
 
   // Create a new transaction and specify the store and data privileges.
@@ -52,45 +56,49 @@ export const postDb = async (name, email, phone, profile) => {
   const store = tx.objectStore('contacts');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+  const request = store.add({ name: name, email: email, phone: phone, profile: profile});
 
   // Get confirmation of the request.
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
+// EXPORTED DELETE function
+
 export const deleteDb = async (id) => {
   console.log('DELETE from the database', id);
 
-  // Create a connection to the Indexed database and the version we want to use.
+  // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('contact_db', 1);
 
-  // Create a new transation and specify the store and data privileges.
+  // Create a new transaction and specify the store and data privileges.
   const tx = contactDb.transaction('contacts', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('contacts');
 
-  // Use the .delete() method to get all the data in the database.
+  // Use the .delete() method to get all data in the database.
   const request = store.delete(id);
 
   // Get confirmation of the request.
-  const results = await request;
-  console.log('results.value', result);
+  const result = await request;
+  console.log('result.value', result);
   return result?.value;
 };
 
+// EXPORTED EDIT function
 export const editDb = async (id, name, email, phone, profile) => {
-  console.log('POST to the database');
+  console.log('PUT to the database');
 
   const contactDb = await openDB('contact_db', 1);
 
   const tx = contactDb.transaction('contacts', 'readwrite');
 
   const store = tx.objectStore('contacts');
-
-  const request = store.put({name: name, email: email, phone: phone, profile: profile});
-
+  
+  const request = store.put({ id: id, name: name, email: email, phone: phone, profile: profile });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-}
+};
+
+
